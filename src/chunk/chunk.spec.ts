779aca346d6bee1a46291fab31415e21ee412cc1
Viktor Kuroljov <viktor.konsta@gmail.com>
2018-12-14 23:22:24 +0200

    Implement chunk() function

--- src/chunk/chunk.spec.ts
+++ src/chunk/chunk.spec.ts
@@ -3,7 +3,6 @@ import { chunk } from './chunk'
 
 const arr = [0, 1, 2, 3, 4, 5]
 const falsey = [, null, undefined, false, 0, NaN, '']
-const stubArray = () => []
 
 describe('chunk', () => {
   test('should return chunked arrays', () => {
@@ -24,6 +23,15 @@ describe('chunk', () => {
     ])
   })
 
+  test('should work with big arrays', () => {
+    const bigArr = Array(1000).fill(0).map((item, i) => i)
+
+    const actual = chunk(bigArr, 3)
+    const expected = _.chunk(bigArr, 3)
+
+    expect(actual).toMatchObject(expected)
+  })
+
   test('should treat falsey `size` values, except `undefined`, as `0`', () => {
     const expected = _.map(falsey, (value) =>
       (value === undefined) ? [[0], [1], [2], [3], [4], [5]] : [],
@@ -36,22 +44,40 @@ describe('chunk', () => {
     expect(actual).toMatchObject(expected)
   })
 
-  test.skip('should ensure the minimum `size` is `0`', () => {
+  test('should ensure the minimum `size` is `0`', () => {
     const values = _.reject(falsey, _.isUndefined).concat(-1, -Infinity)
-    const expected = _.map(values, stubArray)
+    const expected = _.map(values, () => [])
 
     const actual = _.map(values, (n) => chunk(arr, n))
 
     expect(actual).toMatchObject(expected)
   })
 
-  test.skip('should coerce `size` to an integer', () => {
-    expect(chunk(arr, arr.length / 4)).toMatchObject([[0], [1], [2], [3], [4], [5]])
+  describe('size as integer', () => {
+    test('should treat 0.5 as 0', () => {
+      expect(chunk(arr, 0.5)).toMatchObject([])
+    })
+
+    test('should treat 1.5 as 1', () => {
+      expect(chunk(arr, 1.5)).toMatchObject([[0], [1], [2], [3], [4], [5]])
+    })
+
+    test('should treat 3.99 as 3', () => {
+      expect(chunk(arr, 3.99)).toMatchObject([
+        [0, 1, 2],
+        [3, 4, 5],
+      ])
+    })
   })
 
-  test.skip('should work as an iteratee for methods like `_.map`', () => {
-    const actual = _.map([[1, 2], [3, 4]], chunk)
+  test('should work as an iteratee for methods like `_.map`', () => {
+    const actual = _.map([[1, 2], [3, 4], [5, 6], [7, 8]], chunk)
 
-    expect(actual).toMatchObject([[[1], [2]], [[3], [4]]])
+    expect(actual).toMatchObject([
+      [[1], [2]],
+      [[3], [4]],
+      [[5], [6]],
+      [[7], [8]],
+    ])
   })
 })
--- src/chunk/chunk.ts
+++ src/chunk/chunk.ts
@@ -1,11 +1,23 @@
-export function chunk(input: any[], size: number): any[][] {
-  const arr = []
+export function chunk(input: any[], size?: any, _?: any): any[][] {
+  const arr: any[] = []
 
-  for (let i = 0, len = input.length; i < len; i += size) {
-    const start = i
-    const end = (size * i) || size
+  if (_) {
+    size = 1
+  }
 
-    arr.push(input.slice(start, end))
+  if (size >= 2) {
+    size = Math.floor(size)
+
+    for (let i = 0, len = input.length; i < len; i += size) {
+      const end = i + size
+
+      arr.push(input.slice(i, end))
+    }
+  } else if (
+    (size === undefined) ||
+    (size >= 1 && size < 2)
+  ) {
+    return input.map((item) => [item])
   }
 
   return arr
