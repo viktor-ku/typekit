bb83be9cd864956590f55a1d7624ebf201051638
Viktor Kuroljov <viktor.konsta@gmail.com>
2018-12-26 23:12:53 +0200

    Fix difference perf titles

--- src/difference/difference.perf.ts
+++ src/difference/difference.perf.ts
@@ -4,14 +4,14 @@ import { Event, Suite } from 'benchmark'
 import { difference as lodashDifference } from 'lodash'
 import { difference } from './difference'
 
-const a = [1, 2, 3, 4, 3, 2, 4, 3, 4, 5, 4, 5]
+const a = [, 1, 2, 3, 4, 3, 2, 4, 3, 4, 5, 4, 5]
 const b = [6, 5, 4, 5, 6, 5, 4, 3, 2, 3, 4, 3, 3, 4, 5, 4, null]
 
 new Suite()
-  .add('my chunk', () => {
+  .add('my difference', () => {
     difference(a, b)
   })
-  .add('lodash chunk', () => {
+  .add('lodash difference', () => {
     lodashDifference(a, b)
   })
   .on('cycle', (e: Event) => {
