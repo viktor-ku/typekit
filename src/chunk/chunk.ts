1aa5d6b7a3a54246e60263877072ae35d02c6ed4
Viktor Kuroljov <viktor.konsta@gmail.com>
2018-12-27 04:26:23 +0200

    perf: chunk: remember withoutLast var

--- src/chunk/chunk.ts
+++ src/chunk/chunk.ts
@@ -22,12 +22,15 @@ export function chunk<T>(input: T[], size?: number, collection?: any[]): T[][] {
   if (size >= 1) {
     let i = 0
     const len = input.length
+    const withoutLast = len - size
 
-    while (i < len - size) {
+    // Push full slices
+    while (i < withoutLast) {
       arr.push(slice(input, i, size))
       i += size
     }
 
+    // Push last slice (if there is one) that can be either full or partial
     if (len - i > 0) {
       arr.push(slice(input, i, len - i))
     }
